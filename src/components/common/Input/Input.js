5c37fb516f3fba236a12354506e22bef47c47909
Veronica MIHAI <mihaiveronica@ymail.com>
2021-01-15 12:04:51 +0200

    add custom input component with validation, label and error message

---
+++ src/components/common/Input/Input.js
@@ -0,0 +1,63 @@
+import React, { memo } from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+
+function Input({
+  type,
+  inputValue,
+  touched,
+  fieldName,
+  onFieldChanged,
+  onBlur,
+  validator,
+}) {
+  const hasError = touched && !validator(inputValue);
+
+  const fieldClasses = classnames({
+    "c-field": true,
+    "c-error": hasError,
+  });
+
+  const handleFieldChanged = (e) => {
+    onFieldChanged({ name: fieldName, value: e.target.value });
+  };
+
+  const handleBlur = () => {
+    onBlur(fieldName);
+  };
+
+  return (
+    <div className={fieldClasses}>
+      <label htmlFor={fieldName} className="c-label">
+        <strong> {fieldName}</strong>
+        <input
+          data-testid="custom-input"
+          type={type}
+          name={fieldName}
+          className="c-input"
+          onBlur={handleBlur}
+          onChange={handleFieldChanged}
+          value={inputValue}
+        />
+      </label>
+      {hasError && (
+        <span data-testid="input-error" className="c-error">
+          Please fill in {type} for {fieldName}
+        </span>
+      )}
+    </div>
+  );
+}
+
+Input.propTypes = {
+  fieldName: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["text", "number"]).isRequired,
+  inputValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+    .isRequired,
+  touched: PropTypes.bool.isRequired,
+  onFieldChanged: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired,
+  validator: PropTypes.func.isRequired,
+};
+
+export default memo(Input);
---
+++ src/components/common/Input/Input.test.js
@@ -0,0 +1,140 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import {
+  defaultStringValidator,
+  positiveIntegerValidator,
+} from "../../../services/validators.service";
+
+import Input from "./Input.js";
+
+const mockFunc = () => {};
+const inputTestId = "custom-input";
+const inputErrorId = "input-error";
+
+test("text inputs can be created", () => {
+  const textInput = () =>
+    render(
+      <Input
+        fieldName="name"
+        type="text"
+        inputValue="apple"
+        touched={false}
+        onFieldChanged={mockFunc}
+        onBlur={mockFunc}
+        validator={defaultStringValidator}
+      />
+    );
+  expect(textInput).not.toThrowError();
+});
+
+test("number inputs can be created", () => {
+  const numberInput = () =>
+    render(
+      <Input
+        fieldName="value"
+        type="number"
+        inputValue="10"
+        touched={false}
+        onFieldChanged={mockFunc}
+        onBlur={mockFunc}
+        validator={positiveIntegerValidator}
+      />
+    );
+  expect(numberInput).not.toThrowError();
+});
+
+test("other input types which are not text or number throw an error ", () => {
+  const fileInput = () =>
+    render(
+      <Input
+        fieldName="your CV"
+        type="file"
+        inputValue={{}}
+        touched={false}
+        onFieldChanged={mockFunc}
+        onBlur={mockFunc}
+        validator={defaultStringValidator}
+      />
+    );
+
+  expect(fileInput).toThrowError();
+});
+
+test("negative numbers are not allowed in number type inputs", () => {
+  const { getByTestId } = render(
+    <Input
+      fieldName="value"
+      type="number"
+      inputValue=""
+      touched={false}
+      onFieldChanged={mockFunc}
+      onBlur={mockFunc}
+      validator={positiveIntegerValidator}
+    />
+  );
+
+  const inputField = getByTestId(inputTestId);
+  fireEvent.change(inputField, { target: { value: -23 } });
+
+  expect(inputField.value).toBe("");
+});
+
+test("input value is reset when empty inputValue is passed in", () => {
+  const { getByTestId, rerender } = render(
+    <Input
+      fieldName="value"
+      type="number"
+      inputValue="20"
+      touched={false}
+      onFieldChanged={mockFunc}
+      onBlur={mockFunc}
+      validator={positiveIntegerValidator}
+    />
+  );
+
+  const inputField = getByTestId(inputTestId);
+
+  expect(inputField.value).toBe("20");
+
+  rerender(<Input inputValue="" />);
+
+  expect(inputField.value).toBe("");
+});
+
+test("an error message is shown if input has been touched and is invalid, i.e. empty", () => {
+  const { getByTestId } = render(
+    <Input
+      fieldName="value"
+      type="number"
+      inputValue=""
+      touched={true}
+      onFieldChanged={mockFunc}
+      onBlur={mockFunc}
+      validator={positiveIntegerValidator}
+    />
+  );
+
+  const errorElement = getByTestId(inputErrorId);
+
+  expect(errorElement.textContent.trim()).toBe(
+    "Please fill in number for value"
+  );
+});
+
+test("the error message cannot be found if the input value is valid", () => {
+  const { getByTestId } = render(
+    <Input
+      fieldName="value"
+      type="number"
+      inputValue="10"
+      touched={true}
+      onFieldChanged={mockFunc}
+      onBlur={mockFunc}
+      validator={positiveIntegerValidator}
+    />
+  );
+
+  const errorElement = () => getByTestId(inputErrorId);
+
+  expect(errorElement).not.toThrowError();
+});
