36433e570421640e90c93507ec09016b5e8f1209
Veronica MIHAI <mihaiveronica@ymail.com>
2021-01-15 18:17:33 +0200

    render residence form, implement onResidenceAdd function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+
+// Services
+import useGetResidences from "./hooks/get-residences";
+import ResidenceService from "./api/residence.service";
+
+// Components
+import ResidenceForm from "./components/ResidenceForm/ResidenceForm";
 
 import "./App.scss";
 
+const filename = "App.js";
+
 function App() {
-  const defaultState = {
-    residences: [],
+  const { residences, loading, error, setState } = useGetResidences(filename);
+
+  const onAddResidence = (newResidence) => {
+    const onSuccess = () => {
+      setState({
+        residences: [...residences, newResidence],
+        loading: false,
+        error: false,
+      });
+    };
+
+    const onError = () => {
+      setState({ residences, loading: false, error: true });
+    };
+
+    setState({ residences, loading: true, error: false });
+    ResidenceService.addResidence(newResidence).then(onSuccess).catch(onError);
   };
 
-  const [state, setState] = useState(defaultState);
+  if (error) {
+    return (
+      <div data-testid="app-error-msg" className="c-error">
+        Something went wrong, please try again later!
+      </div>
+    );
+  }
+
+  if (loading) {
+    return (
+      <div className="loader__container">
+        <div data-testid="app-loader" className="c-spinner c-spinner--l" />{" "}
+      </div>
+    );
+  }
 
   return (
     <div className="app__container">
@@ -15,8 +53,9 @@ function App() {
         <h1> Residence App </h1>
       </header>
       <main>
+        <ResidenceForm onAddResidence={onAddResidence} />
       </main>
-      <footer>Created by @Veronica Mihai 2020 with ☕ and 🎵</footer>
+      <footer>Created by @Veronica Mihai 2021 with ☕ and 🎵</footer>
     </div>
   );
 }
