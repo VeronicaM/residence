25867abf0e209d4fb9d9cd70ae7d6575b37c4611
Veronica MIHAI <mihaiveronica@ymail.com>
2021-01-15 18:15:32 +0200

    add custom hook for getting residences data

---
+++ src/hooks/get-residences.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import LoggerService from "../core/services/logger.service.js";
+import ResidenceService from "../api/residence.service";
+
+const defaultState = {
+  residences: [],
+  loading: false,
+  error: false,
+};
+
+export default function useGetResidences(filename) {
+  const [state, setState] = useState(defaultState);
+
+  useEffect(() => {
+    const getResidencesAbortController = new AbortController();
+
+    async function fetchResidences() {
+      try {
+        setState({ ...state, loading: true });
+
+        const residences = await ResidenceService.getResidences({
+          abortController: getResidencesAbortController,
+        });
+
+        setState({ ...state, residences, loading: false });
+      } catch (err) {
+        setState({ ...state, loading: false, error: true });
+
+        LoggerService.log({
+          message: JSON.stringify(err),
+          type: LoggerService.MESSAGE_TYPES.WARNING,
+          filename,
+        });
+      }
+    }
+
+    fetchResidences();
+
+    return () => {
+      getResidencesAbortController.abort();
+    };
+  }, []);
+
+  return { ...state, setState };
+}
