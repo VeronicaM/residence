16dba1cd39b9eb242035f3fcc0a5840f6311b4ff
Veronica MIHAI <mihaiveronica@ymail.com>
2021-01-16 10:08:07 +0200

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 
-xtest("renders header text", () => {
+test("renders header text", async () => {
   render(<App />);
-  const headerText = screen.getByText(/Residence App/i);
-  expect(headerText).toBeInTheDocument();
+
+  await waitFor(() =>
+    expect(screen.getByText(/Residence App/i)).toBeInTheDocument()
+  );
 });
